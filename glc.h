d28298149ff370f2f1cba472a4c0cc1ffa2f6a12
Vit than yeu <111245077+zitkon@users.noreply.github.com>
2022-09-09 19:50:06 +0700

    Add files via upload

---
+++ glc.h
@@ -0,0 +1,10 @@
+/*glc.h*/
+#if !defined GLC
+#define _GLC_
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include <math.h>
+#define PI 3.14159265
+typedef struct {float x; float y; float z;} vec3;
+typedef struct {int a; int b; int c;} vec3d; //lưu trữ giá trị f trong file obj
+
